ff1670f0e8b310009803b8c9cb167dbbd4ba3e14
Deploy <deploy@sportsphere.fun>
2026-08-16 06:42:53 +0000

    fix: add Android platform files and remove broken pubspec.lock
    - Generated complete android/ directory for Flutter (build.gradle.kts, settings.gradle.kts, AndroidManifest, MainActivity, gradle wrapper)
    - Removed stale pubspec.lock with 7 missing packages
    - Added !Flutter/android/ exception to root .gitignore
    
    After pulling, run:
      cd Flutter && flutter pub get && flutter run

---
+++ Flutter/android/app/build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+android {
+    namespace = "com.sportsphere.app"
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = flutter.ndkVersion
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+
+    defaultConfig {
+        applicationId = "com.sportsphere.app"
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+    buildTypes {
+        release {
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+}
+
+flutter {
+    source = "../.."
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+}
